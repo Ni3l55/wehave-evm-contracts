67de8fb7998f78ab7f0a5b51f479bff4dd254197
Niels Vandeweyer <vandeweyer.niels@gmail.com>
2022-12-01 18:27:01 +0100

    erc1155 tested and okay

--- contracts/ItemNFT.sol
+++ contracts/ItemNFT.sol
@@ -7,8 +7,21 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
+interface USDC {
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
 contract ItemNFT is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
-    constructor() ERC1155("") {}
+
+    USDC public usdc;
+    uint256 constant usdcDecimals = 18;
+
+    uint256 public mintPrice = 600;
+    uint256 public mintingFee = 30; // 5% fee on crowdfund
+
+    constructor() ERC1155("") {
+      usdc = USDC(0x0FA8781a83E46826621b3BC094Ea2A0212e71B23);
+    }
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
@@ -23,17 +36,20 @@ contract ItemNFT is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
     }
 
     function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
+        private
     {
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
+    function mintWithUSDC(uint256 id, uint256 amount)
+      public
     {
-        _mintBatch(to, ids, amounts, data);
+      // Transfer USDC from the account to this contract
+      uint256 price = amount * (mintPrice + mintingFee);
+      usdc.transferFrom(msg.sender, address(this), price * 10 ** usdcDecimals);
+
+      // Perform mint
+      mint(msg.sender, id, amount, "");
     }
 
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
